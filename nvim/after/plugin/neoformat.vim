8f4de9b05a264dcd7fa785025a281734eaf79b2f
Ali Ghorab <alighorab88@gmail.com>
2022-09-22 07:20:17 +0200

    config neoformat and adding nvim-tree

---
+++ nvim/after/plugin/neoformat.vim
@@ -0,0 +1,1 @@
+let g:shfmt_opt="-ci"
---
+++ nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,46 @@
+local Remap = require("keymap")
+local nnoremap = Remap.nnoremap
+local silent = { silent = true }
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    auto_reload_on_write = true,
+    view = {
+        adaptive_size = false,
+        relativenumber = true,
+        float = {
+            enable = true,
+            open_win_config = {
+                relative = "editor",
+                border = "rounded",
+                width = 60,
+                height = 24,
+                row = 8,
+                col = 60,
+            }
+        }
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+        custom = { "^.git$" }
+    },
+    diagnostics = {
+        enable = true,
+        icons = { hint = "", info = "", warning = "", error = "" },
+        show_on_dirs = true
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true
+        }
+    }
+})
+
+nnoremap("<C-->", ":NvimTreeToggle<CR>", silent)
