7740a51b29585044c7381710f15fc2a3bc06fa2e
Ali Ghorab <alighorab88@gmail.com>
2022-12-14 15:40:38 +0200

    add flake8 linter

--- nvim/after/plugin/lsp/null-ls.lua
+++ nvim/after/plugin/lsp/null-ls.lua
@@ -8,14 +8,24 @@ local formatters = {
 	"prettier",
 }
 
+local linters = {
+    "flake8",
+}
+
+local ensure_installed = vim.tbl_flatten(formatters)
+for _, linter in ipairs(linters) do
+    table.insert(ensure_installed, linter)
+end
+
 require("mason-null-ls").setup({
-	ensure_installed = formatters,
+	ensure_installed = ensure_installed,
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
+        -- Formatters
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.shfmt.with({
@@ -25,6 +35,9 @@ null_ls.setup({
 		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.gofumpt,
 		null_ls.builtins.formatting.prettier,
+
+        -- Linters
+        null_ls.builtins.diagnostics.flake8,
 	},
 })
 
