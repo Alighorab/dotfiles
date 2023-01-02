53d15b06c962111cd5ed24ae43da04e33856efbc
Ali Ghorab <alighorab88@gmail.com>
2023-01-02 06:32:39 +0200

    perf: remove unnecessary files and plugins
    - move fugitive remaps to lazy.lua
    - remove lspsaga, playground, and vim-matchup

--- nvim/after/plugin/git.lua
+++
@@ -1,7 +0,0 @@
-local Remap = require("logan.utils.keymap")
-local nnoremap = Remap.nnoremap
--- Git
-nnoremap("<leader>gg", vim.cmd.Git)
-nnoremap("<leader>gd", function()
-  vim.cmd.Git("diff %")
-end)
--- nvim/ftplugin/rust.lua
+++
@@ -1,8 +0,0 @@
-local Remap = require("logan.utils.keymap")
-local nnoremap = Remap.nnoremap
-local bufopts = { silent = true, buffer = true }
-
-nnoremap("<F6>", function()
-  vim.cmd.write()
-  vim.cmd("!cargo build")
-end, bufopts)
