fd03675c7597f383eb703235c5024eb8cb4fa30c
Ali Ghorab <alighorab88@gmail.com>
2023-01-02 06:36:11 +0200

    fix: snippets path

--- nvim/after/plugin/lsp/snippets.lua
+++ nvim/after/plugin/lsp/snippets.lua
@@ -1,7 +1,7 @@
 -- Snippets
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = {
-    vim.env.HOME .. "/.local/share/nvim/site/pack/packer/start/friendly-snippets/",
+    vim.env.HOME .. "/.local/share/nvim/lazy/friendly-snippets/",
   },
   include = nil, -- Load all languages
   exclude = {},
