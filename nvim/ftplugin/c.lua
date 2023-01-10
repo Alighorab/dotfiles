4ad0fc4762954cd2080544a6b5e1dc5186629f71
Ali Ghorab <alighorab88@gmail.com>
2023-01-10 11:40:46 +0200

    feat(nvim): add `compile` or `make` keymap for `c` files

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -3,3 +3,51 @@ vim.keymap.set("n", "<leader>gh", vim.cmd.ClangdSwitchSourceHeader, {
   buffer = true,
   desc = "Clang Switch Source Header",
 })
+
+local make = function()
+  local uv = vim.loop
+  local stdout = uv.new_pipe()
+  local stderr = uv.new_pipe()
+
+  uv.spawn("make", {
+    stdio = { stdout, stderr },
+  })
+
+  uv.read_start(stdout, function(err, data)
+    assert(not err, err)
+    if data then
+      print(data)
+    end
+  end)
+
+  uv.read_start(stderr, function(err, data)
+    assert(not err, err)
+    if data then
+      print(data)
+    end
+  end)
+end
+
+local compile = function()
+  local uv = vim.loop
+  local cmd = "clang"
+  local args = {
+    "-Wall",
+    "-g",
+    vim.fn.expand("%:t"),
+    "-o",
+    vim.fn.expand("%:t:r"),
+  }
+  print(cmd, table.concat(args, " "))
+  uv.spawn(cmd, {
+    args = args,
+  })
+end
+
+vim.keymap.set("n", "<F6>", function()
+  if vim.fn.filereadable("makefile") == 1 or vim.fn.filereadable("Makefile") == 1 then
+    make()
+  else
+    compile()
+  end
+end)
