6a96232308afcf0a0dcaf855ec824e04155f4708
Ali Ghorab <alighorab88@gmail.com>
2023-02-12 22:57:38 +0200

    update signs

--- nvim/after/plugin/lsp/init.lua
+++ nvim/after/plugin/lsp/init.lua
@@ -115,7 +115,7 @@ local lsp_flags = {
 }
 
 -- Signs
-local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "" }
+local signs = { Error = "✘", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
