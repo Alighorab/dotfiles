950dd309f3c44e445b69507e13e02417179aae3e
Ali Ghorab <alighorab88@gmail.com>
2023-04-14 22:11:18 +0200

    fix(nvim): update `harpoon` keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -6,18 +6,18 @@ vim.keymap.set("n", "<leader>hu", function()
   require("harpoon.ui").toggle_quick_menu()
 end, { silent = true, desc = "Harppon Quick Menu Toggle" })
 
-vim.keymap.set("n", "<A-1>", function()
+vim.keymap.set("n", "<C-j>", function()
   require("harpoon.ui").nav_file(1)
 end, { silent = true, desc = "Harppon Navigate File (1)" })
 
-vim.keymap.set("n", "<A-2>", function()
+vim.keymap.set("n", "<C-f>", function()
   require("harpoon.ui").nav_file(2)
 end, { silent = true, desc = "Harppon File (2)" })
 
-vim.keymap.set("n", "<A-3>", function()
+vim.keymap.set("n", "<C-h>", function()
   require("harpoon.ui").nav_file(3)
 end, { silent = true, desc = "Harppon File (3)" })
 
-vim.keymap.set("n", "<A-4>", function()
+vim.keymap.set("n", "<C-g>", function()
   require("harpoon.ui").nav_file(4)
 end, { silent = true, desc = "Harppon File (4)" })
