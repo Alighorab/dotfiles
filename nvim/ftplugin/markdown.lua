2bed2274cebcf04eee3cc2ffd4eb9b5e4b785f3e
Ali Ghorab <alighorab88@gmail.com>
2023-04-26 11:11:29 +0200

    fix(nvim): remove unnedded files

--- nvim/ftplugin/markdown.lua
+++
@@ -1,4 +0,0 @@
-local peek = require("peek")
-
-vim.keymap.set("n", "<leader>pp", peek.open, { buffer = true, desc = "Peek Open Preview" })
-vim.keymap.set("n", "<leader>pc", peek.close, { buffer = true, desc = "Peek Close Preview" })
