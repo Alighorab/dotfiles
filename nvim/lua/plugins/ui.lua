e9d2a3ec612aca26f66416a0edcc82be7a4efd3a
Ali Ghorab <alighorab88@gmail.com>
2023-05-25 00:56:47 +0300

    feat(nvim): change `dashboard` header 3>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -31,12 +31,15 @@ return {
     opts = function()
       local dashboard = require("alpha.themes.dashboard")
       dashboard.section.header.val = {
-        " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
-        " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
-        " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
-        " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
-        " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
-        " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+        "           ▄ ▄                   ",
+        "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+        "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+        "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+        "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+        "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+        "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+        "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+        "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
       }
       dashboard.section.header.opts.hl = "DashboardHeader"
 
@@ -50,8 +53,12 @@ return {
       }
 
       dashboard.config.layout[1].val = vim.fn.max({ 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) })
-      dashboard.config.layout[3].val = 5
+      dashboard.config.layout[3].val = 3
       dashboard.config.opts.noautocmd = true
+
+      vim.cmd("au WinEnter,FileType alpha set cmdheight=0")
+      vim.cmd("au WinLeave,FileType alpha set cmdheight=1")
+
       return dashboard
     end,
     config = function()
@@ -76,9 +83,5 @@ return {
         -- refer to the configuration section below
       })
     end,
-    cmd = {
-      "TodoQuickFix",
-      "TodoTelescope",
-    },
   },
 }
