c10fd3f25f0f0abaea19478f7306d35dcd5f382e
Ali Ghorab <alighorab88@gmail.com>
2023-05-31 04:16:58 +0300

    feat(nvim): pick program name using `telescope`

--- nvim/lua/plugins/debug-adapters/codelldb.lua
+++ nvim/lua/plugins/debug-adapters/codelldb.lua
@@ -3,6 +3,11 @@ local M = {}
 M.setup = function()
   local dap = require("dap")
   local dap_vscode = require("dap.ext.vscode")
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
 
   dap.adapters.codelldb = {
     type = "server",
@@ -19,7 +24,23 @@ M.setup = function()
       type = "codelldb",
       request = "launch",
       program = function()
-        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        return coroutine.create(function(coro)
+          local opts = {}
+          pickers
+              .new(opts, {
+                prompt_title = "Path to executable",
+                finder = finders.new_oneshot_job({ "fd", "--hidden", "--no-ignore", "--type", "x" }, {}),
+                sorter = conf.generic_sorter(opts),
+                attach_mappings = function(buffer_number)
+                  actions.select_default:replace(function()
+                    actions.close(buffer_number)
+                    coroutine.resume(coro, action_state.get_selected_entry()[1])
+                  end)
+                  return true
+                end,
+              })
+              :find()
+        end)
       end,
       cwd = "${workspaceFolder}",
       -- stopAtEntry = true,
@@ -38,7 +59,23 @@ M.setup = function()
       type = "codelldb",
       request = "launch",
       program = function()
-        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        return coroutine.create(function(coro)
+          local opts = {}
+          pickers
+              .new(opts, {
+                prompt_title = "Path to executable",
+                finder = finders.new_oneshot_job({ "fd", "--hidden", "--no-ignore", "--type", "x" }, {}),
+                sorter = conf.generic_sorter(opts),
+                attach_mappings = function(buffer_number)
+                  actions.select_default:replace(function()
+                    actions.close(buffer_number)
+                    coroutine.resume(coro, action_state.get_selected_entry()[1])
+                  end)
+                  return true
+                end,
+              })
+              :find()
+        end)
       end,
       args = function()
         local argv = {}
