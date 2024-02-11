de7cdecfaca9f7744759d04bed26970e1f1df4e5
Ali Ghorab <alighorab88@gmail.com>
2024-02-12 00:50:00 +0200

    feat: add overseer to `heirline`

--- nvim/lua/plugins/heirline.lua
+++ nvim/lua/plugins/heirline.lua
@@ -467,6 +467,52 @@ return {
       provider = "%#None#",
     }
 
+    local function rpad(child)
+      return {
+        condition = child.condition,
+        child,
+      }
+    end
+    local function OverseerTasksForStatus(status)
+      return {
+        condition = function(self)
+          return self.tasks[status]
+        end,
+        provider = function(self)
+          return string.format("%s%d ", self.symbols[status], #self.tasks[status])
+        end,
+        hl = function()
+          return {
+            fg = utils.get_highlight(string.format("Overseer%s", status)).fg,
+          }
+        end,
+      }
+    end
+
+    local Overseer = {
+      condition = function()
+        return package.loaded.overseer
+      end,
+      init = function(self)
+        local tasks = require("overseer.task_list").list_tasks({ unique = true })
+        local tasks_by_status = require("overseer.util").tbl_group_by(tasks, "status")
+        self.tasks = tasks_by_status
+      end,
+      static = {
+        symbols = {
+          ["CANCELED"] = " ",
+          ["FAILURE"] = "󰅚 ",
+          ["SUCCESS"] = "󰄴 ",
+          ["RUNNING"] = "󰑮 ",
+        },
+      },
+
+      rpad(OverseerTasksForStatus("CANCELED")),
+      rpad(OverseerTasksForStatus("RUNNING")),
+      rpad(OverseerTasksForStatus("SUCCESS")),
+      rpad(OverseerTasksForStatus("FAILURE")),
+    }
+
     local Statusline = {
       ViMode,
       Git,
@@ -478,6 +524,7 @@ return {
       Align,
 
       Navic,
+      Overseer,
       LSPActive,
       FileType,
       Ruler,
